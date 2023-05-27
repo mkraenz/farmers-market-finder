35e00f35b62a2b7036f79d74eff1e7ce71fbb033
Mirco Kraenz <software@kraenz.eu>
2023-05-27 22:57:14 +0200

    feat: jpg file upload to s3 proxied by nestjs

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { S3Service } from './s3.service';
+
+const oneMegabyte = 1024 * 1024;
 
 @Controller('s3')
 export class S3Controller {
@@ -8,5 +20,22 @@ export class S3Controller {
   @Get()
   async listBuckets() {
     return this.svc.listBuckets();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 10 * oneMegabyte }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    await this.svc.putImage(file.originalname, file.buffer);
+    return { message: 'File uploaded successfully' };
   }
 }
--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -8,13 +8,16 @@ const logger = new Logger('S3Service');
 @Injectable()
 export class S3Service {
   private client: S3.S3Client;
-  constructor(cfg: ConfigService<Environment, true>) {
+  private bucket: string;
+
+  constructor(private cfg: ConfigService<Environment, true>) {
     // region: string, // credentials: { accessKeyId: string; secretAccessKey: string },
     const credentials = {
       accessKeyId: cfg.get('AWS_ACCESS_KEY_ID'),
       secretAccessKey: cfg.get('AWS_SECRET_ACCESS_KEY'),
     };
     const region = cfg.get('AWS_REGION');
+    this.bucket = cfg.get('AWS_S3_BUCKET');
     this.client = new S3.S3Client({
       credentials,
       region,
@@ -22,15 +25,30 @@ export class S3Service {
   }
 
   async listBuckets() {
-    const command = new S3.ListBucketsCommand({});
     try {
+      const command = new S3.ListBucketsCommand({});
       const data = await this.client.send(command);
       logger.log(JSON.stringify(data, null, 2));
       return data.Buckets;
     } catch (error) {
       logger.error(error);
-    } finally {
-      // finally.
+    }
+  }
+
+  /** TODO the whole Nestjs-as-proxy-to-S3 for uploads can be avoided by using presigned URLs https://docs.aws.amazon.com/AmazonS3/latest/userguide/PresignedUrlUploadObject.html */
+  async putImage(key: string, body: string | Buffer) {
+    try {
+      const command = new S3.PutObjectCommand({
+        Bucket: this.bucket,
+        Key: key,
+        Body: body,
+        ContentEncoding: 'base64',
+        ContentType: 'image/jpeg',
+      });
+      const res = await this.client.send(command);
+      logger.log(JSON.stringify(res, null, 2));
+    } catch (error) {
+      logger.error(error);
     }
   }
 }
