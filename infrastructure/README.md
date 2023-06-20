64fec50c8a16197283bea8d5dd3baf4670da9429
Mirco Kraenz <software@kraenz.eu>
2023-06-20 09:47:41 +0200

    wip: push to ECR

---
+++ infrastructure/README.md
@@ -0,0 +1,24 @@
+# Infrastructure for Farmers Market Finder
+
+```sh
+# from infrastructure/ directory
+TAG_SUFFIX=latest
+TAG_REPO=farmers-market-finder
+docker build -t $TAG_REPO:$TAG_SUFFIX ..
+
+# test the build
+docker run --rm -p 3001:3333 -e PORT='3333' -e POSTGRES_HOST='172.17.0.1' -e POSTGRES_PORT=5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=test --name nestjs-fargate-test $TAG_REPO:$TAG_SUFFIX
+google-chrome --profile-directory=Default http://localhost:3001
+
+# login to ECR
+ECR_REGISTRY_BASE=756399734264.dkr.ecr.us-east-1.amazonaws.com
+ECR_REPOSITORY_SUFFIX=repo-cb2e401
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY_BASE
+
+# tag the image for ECR
+TAG_ID=$(docker images -q $TAG_REPO:$TAG_SUFFIX)
+docker tag $TAG_ID $ECR_REGISTRY_BASE/$ECR_REPOSITORY_SUFFIX:$TAG_SUFFIX
+
+# push the image to ECR
+docker push $ECR_REGISTRY_BASE/$ECR_REPOSITORY_SUFFIX:$TAG_SUFFIX
+```
--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -15,8 +15,42 @@ export const imageRepoArn = imageRepo.arn;
 export const imageRepoName = imageRepo.name;
 export const imageRepoUrl = imageRepo.repositoryUrl;
 
+const vpc = new aws.ec2.Vpc("vpc", {
+    cidrBlock: "10.0.0.0/16",
+    tags
+  });
 
-// const vpc = new aws.ec2.Vpc("vpc", {
-//     cidrBlock: "10.0.0.0/16",
-//     tags
-//   });
+
+//   const example = new aws.apprunner.Service("example", {
+//     serviceName: "example",
+//     sourceConfiguration: {
+//         authenticationConfiguration: {
+//             connectionArn: aws_apprunner_connection.example.arn,
+//         },
+//         codeRepository: {
+//             codeConfiguration: {
+//                 codeConfigurationValues: {
+//                     buildCommand: "python setup.py develop",
+//                     port: "8000",
+//                     runtime: "PYTHON_3",
+//                     startCommand: "python runapp.py",
+//                 },
+//                 configurationSource: "API",
+//             },
+//             repositoryUrl: "https://github.com/example/my-example-python-app",
+//             sourceCodeVersion: {
+//                 type: "BRANCH",
+//                 value: "main",
+//             },
+//         },
+//     },
+//     networkConfiguration: {
+//         egressConfiguration: {
+//             egressType: "VPC",
+//             vpcConnectorArn: aws_apprunner_vpc_connector.connector.arn,
+//         },
+//     },
+//     tags: {
+//         Name: "example-apprunner-service",
+//     },
+// });
